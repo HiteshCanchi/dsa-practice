29dd3febc10d195b55262d2d12b052e83355f223
HiteshCanchi <hitesh.canchi@outlook.com>
2025-06-29 19:41:04 +0530

    Add files via upload

---
+++ sumNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    
+    cout<<"enter a: ";
+    cin>>a;
+
+    cout<<"enter b: ";
+    cin>>b;
+
+    int sum = a+b;
+    
+    cout<<"the sum is: "<<sum;
+    return 0;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double val = 100.99;
+
+    int newPrice = (int)val;
+    cout<<newPrice;
+    
+    return 0;
+}
