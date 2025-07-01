6e40df81c9af509424c01dc7181a0d99d6fc2dd4
HiteshCanchi <hitesh.canchi@outlook.com>
2025-07-01 14:14:24 +0530

    Add files via upload

---
+++ whileLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,i=1;
+    cout<<"enter n: ";
+    cin>>n;
+
+    while(i<=n) {
+        cout<<i<<" ";
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
